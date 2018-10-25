d7e9703f0fee8d7ed8073fd591738afdec4cb789
ishai.m <ishai.m@taboola.com>
2018-10-25 11:25:28 +0300

    bug fix: prevent an error caused by not finding main.js by default

--- weekNumberExtentsion/main.js
+++ weekNumberExtentsion/main.js
@@ -1,14 +1,22 @@
-Date.prototype.getWeek = function () {
-    let target  = new Date(this.valueOf());
-    let dayNr   = (this.getDay() + 6) % 7;
-    target.setDate(target.getDate() - dayNr + 3);
-    let firstThursday = target.valueOf();
-    target.setMonth(0, 1);
-    if (target.getDay() != 4) {
-        target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+try {
+    Date.prototype.getWeek = function () {
+        let target = new Date(this.valueOf());
+        let dayNr = (this.getDay() + 6) % 7;
+        target.setDate(target.getDate() - dayNr + 3);
+        let firstThursday = target.valueOf();
+        target.setMonth(0, 1);
+        if (target.getDay() != 4) {
+            target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+        }
+        return 1 + Math.ceil((firstThursday - target) / 604800000);
     }
-    return 1 + Math.ceil((firstThursday - target) / 604800000);
+
+    let d = new Date();
+    if(document.getElementById('weekNumText')) {
+        document.getElementById('weekNumText').innerHTML = d.getWeek();
+    }
+}
+catch (e) {
+    console.log("Error Week number extension: " + e);
 }
 
-let d= new Date();
-document.getElementById('weekNumText').innerHTML = d.getWeek();
